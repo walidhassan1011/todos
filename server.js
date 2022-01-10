6f4f5d3bff40a779df7be2282f0781ef12e06fe1
walidhassan1011 <walidhassan009@gmail.com>
2022-01-10 18:10:47 +0200

    first-commit

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (res, req) => {});
+app.get("/", (res, req) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/todos", (res, req) => {
+  res.json({ todos });
+});
+
+app.post("/todo", (res, req) => {
+  const data = req.body;
+  todos.push(data);
+});
+
+app.post("/delete", (res, req) => {
+  const { id, message } = req.body;
+  todos.filter(id);
+  todos.filter(message);
+});
 
 const PORT = 3000;
-
 app.listen(PORT, () => {
   console.log(`server is running on port:${PORT}`);
 });
